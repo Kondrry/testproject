4bae8b03504c2987fa1519b59a6b67a269c04849
Kondrry <artembuuklan67@gmail.com>
2026-09-15 20:36:43 +0300

    Remove example print statements from main.py
    Removed example print statements for clarity.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import helpers
 
-print(helpers.is_even(4)) # True
-print(helpers.average([1, 2, 3, 4, 5])) # 3.0
-
 def main():
     numbers = [10, 15, 20, 25, 30]
     even_numbers = [num for num in numbers if helpers.is_even(num)]
